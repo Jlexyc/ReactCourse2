8008926de84e2cd59bb166eda5eb090ef227a3dd
Oleksii Kozulin <senior.jlexyc@gmail.com>
2022-07-16 13:19:44 +0300

    Added fetch

--- src/Components/ItemsList/ItemsList.js
+++ src/Components/ItemsList/ItemsList.js
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState, useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useNavigate, useSearchParams } from 'react-router-dom';
 import { ItemComponent } from "../ItemComponent/ItemComponent";
@@ -7,26 +7,44 @@ import { selectCategories } from "../../Store/Category/selectors";
 import { deleteItemAction } from "../../Store/Items/actions";
 
 export const ItemsList = () => {
-  const items = useSelector(selectItems);
-  const categories = useSelector(selectCategories)
+  // const items = useSelector(selectItems);
+  // const categories = useSelector(selectCategories)
   const navigate = useNavigate();
-  const { categoryId } = useParams();
+  // const { categoryId } = useParams();
 
-  const itemsToDisplay = useMemo(() => {
-    let returnItems = [];
-    if (categoryId) {
-      returnItems = items.filter((i) => i.title === categoryId )
-    } else {
-      returnItems = items;
-    }
-    return returnItems.map((item) => {
-      return {
-        ...item,
-        category: categories[item.categoryId].name,
-      }
-    })
-  }, [items, categoryId])
+  const [itemsToDisplay, setItemsToDisplay] = useState([]);
+
+  // const itemsToDisplay = useMemo(() => {
+  //   let returnItems = [];
+  //   if (categoryId) {
+  //     returnItems = items.filter((i) => i.title === categoryId )
+  //   } else {
+  //     returnItems = items;
+  //   }
+  //   return returnItems.map((item) => {
+  //     return {
+  //       ...item,
+  //       category: categories[item.categoryId].name,
+  //     }
+  //   })
+  // }, [items, categoryId])
   
+  useEffect(async () => {
+    // fetch('http://127.0.0.1:8080/goods')
+    //   .then((response) => response.json())
+    //   .then((data) => console.log('data: ', data))
+    //   .catch(error => alert('sorry, something went wrong'))
+
+    try {
+      const response = await fetch('http://127.0.0.1:8080/goods');
+      const data = await response.json()
+      console.log('data: ', data)
+    }
+    catch (error) {
+      alert('sorry, something went wrong')
+    }
+  }, []);
+
   const dispatch = useDispatch();
 
   const onDeleteElement = useCallback(
